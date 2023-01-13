4d5aa472db846b8946e0546bd0c48812a714fbc5
YANGNAVI <76214596+YANGNAVI@users.noreply.github.com>
2023-01-14 00:22:59 +0900

    Add files via upload

--- rosegap.py
+++ rosegap.py
@@ -10,6 +10,7 @@ chat_token = "5963692938:AAEPEQRG5rME9HYUD5NNMgK4pNDbKFL97oQ"
 bot = telegram.Bot(token=chat_token)
 
 symbol = "ROSE"
+bot_url = f"https://api.telegram.org/bot{chat_token}/sendMessage?"
 
 
 def mexc_request_data(url) -> None:
@@ -60,7 +61,9 @@ mexc : {mexc_bid1}
 binance : {binance_data}
               '''
                 print(text)
-                bot.sendMessage(chat_id=-1001895027074, text=text)
+                data = {"chat_id": "-1001895027074", "text": text}
+                res = requests.post(bot_url, json=data)
+
         elif mexc_bid1 >= bingX_ask1:
             gap = round(mexc_bid1 / bingX_ask1 * 100 - 100, 3) * -1
             print("mexc", gap, count)
@@ -73,8 +76,10 @@ mexc : {mexc_bid1}
 binance : {binance_data}
               '''
             print(text)
-            bot.sendMessage(chat_id=-1001895027074, text=text)
+            data = {"chat_id": "-1001895027074", "text": text}
+            res = requests.post(bot_url, json=data)
         time.sleep(5)
     except Exception as e:
-        bot.sendMessage(chat_id=5792701026, text=f"오류발생 : {e}")
+        data = {"chat_id": "5792701026", "text": f"오류발생 : {e}"}
+        res = requests.post(bot_url, json=data)
         time.sleep(3)
